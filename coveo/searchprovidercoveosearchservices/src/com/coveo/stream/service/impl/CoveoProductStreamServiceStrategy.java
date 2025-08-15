16aa62f3f923815e0654795f2b5ec873bfaf97d9
Maxime Gagnon <maxime.gagnon@canac.ca>
2025-08-15 14:08:23 -0400

    feat: add support for partial document updates
    Introduces functionality to handle partial updates of documents in
    the Coveo product streaming service strategy. Adds a mechanism to
    verify document operation types and performs partial updates when
    applicable. This improvement enhances efficiency and reduces redundant
    document processing by only updating fields that have changed.

--- coveo/searchprovidercoveosearchservices/src/com/coveo/stream/service/impl/CoveoProductStreamServiceStrategy.java
+++ coveo/searchprovidercoveosearchservices/src/com/coveo/stream/service/impl/CoveoProductStreamServiceStrategy.java
@@ -1,6 +1,8 @@
 package com.coveo.stream.service.impl;
 
 import com.coveo.pushapiclient.DocumentBuilder;
+import com.coveo.pushapiclient.PartialUpdateDocument;
+import com.coveo.pushapiclient.PartialUpdateOperator;
 import com.coveo.pushapiclient.exceptions.NoOpenFileContainerException;
 import com.coveo.pushapiclient.exceptions.NoOpenStreamException;
 import com.coveo.searchservices.admin.data.CoveoSnCountry;
@@ -24,23 +26,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Currency;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
-import static com.coveo.constants.SearchprovidercoveosearchservicesConstants.COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE;
-import static com.coveo.constants.SearchprovidercoveosearchservicesConstants.COVEO_PRODUCT_STREAM_LOG_INTERVAL_PERCENTAGE;
-import static com.coveo.constants.SearchprovidercoveosearchservicesConstants.COVEO_PRODUCT_STREAM_LOG_INTERVAL_PERCENTAGE_DEFAULT;
-import static com.coveo.constants.SearchprovidercoveosearchservicesConstants.COVEO_URI_TYPE_INDEX_ATTRIBUTE;
+import static com.coveo.constants.SearchprovidercoveosearchservicesConstants.*;
 
 public class CoveoProductStreamServiceStrategy<T extends CoveoStreamService> implements CoveoStreamServiceStrategy {
 
     private static final Logger LOG = Logger.getLogger(CoveoProductStreamServiceStrategy.class);
+
+    private static final String PARTIAL_UPDATE = "PARTIAL_UPDATE";
+    private static final String COVEO_NAME_INDEX_ATTRIBUTE = "name";
 
     List<SnLanguage> languages;
     List<SnCurrency> currencies;
@@ -107,20 +102,41 @@ public class CoveoProductStreamServiceStrategy<T extends CoveoStreamService> imp
 
     private boolean streamDocument(SnDocumentBatchOperationRequest request, SnLanguage language, SnCurrency currency, CoveoSnCountry country, T streamService) {
         boolean success = true;
+        if (request.getOperationType() == null) {
+            LOG.error("Document operation type is null for document " + request.getDocument().getId());
+            return false;
+        }
         if (isApplicableForCountry(request, language, currency, country)) {
             synchronized (streamService) {
-                DocumentBuilder coveoDocument = createCoveoDocument(request.getDocument(), language.getId(), currency.getId());
-                if (coveoDocument != null) {
-                    try {
-                        if (LOG.isDebugEnabled()) {
-                            JsonObject jsonDocument = (new Gson()).toJsonTree(coveoDocument.getDocument()).getAsJsonObject();
-                            LOG.debug("Pushing document: " + jsonDocument.toString());
+                if (PARTIAL_UPDATE.equals(request.getOperationType().getCode())) {
+                    List<PartialUpdateDocument> partialUpdateDocuments = createCoveoPartialDocument(request.getDocument(), language.getId(), currency.getId());
+                    if (!partialUpdateDocuments.isEmpty()) {
+                        try {
+                            if (LOG.isDebugEnabled()) {
+                                LOG.debug("Pushing partial update for document " + request.getDocument().getId());
+                            }
+                            streamService.pushPartialDocument(partialUpdateDocuments);
+                        } catch (IOException | InterruptedException exception) {
+                            success = false;
+                            LOG.error("Failed to index " + request.getDocument().getId(), exception);
                         }
-                        streamService.pushDocument(coveoDocument);
-                    } catch (IOException | InterruptedException exception) {
+                    } else {
+                        LOG.error("Failed to index " + request.getDocument().getId());
                         success = false;
-                        LOG.error("Failed to index " + request.getDocument().getId(), exception);
                     }
+                } else {
+                    DocumentBuilder coveoDocument = createCoveoDocument(request.getDocument(), language.getId(), currency.getId());
+                    if (coveoDocument != null) {
+                        try {
+                            if (LOG.isDebugEnabled()) {
+                                JsonObject jsonDocument = (new Gson()).toJsonTree(coveoDocument.getDocument()).getAsJsonObject();
+                                LOG.debug("Pushing document: " + jsonDocument.toString());
+                            }
+                            streamService.pushDocument(coveoDocument);
+                        } catch (IOException | InterruptedException exception) {
+                            success = false;
+                            LOG.error("Failed to index " + request.getDocument().getId(), exception);
+                        }
                 } else {
                     LOG.error("Failed to index " + request.getDocument().getId());
                     success = false;
@@ -170,41 +186,74 @@ public class CoveoProductStreamServiceStrategy<T extends CoveoStreamService> imp
     }
 
     private DocumentBuilder createCoveoDocument(SnDocument document, String languageIsoCode, String currencyIsoCode) {
-        Locale locale = commonI18NService.getLocaleForIsoCode(languageIsoCode);
-        Currency currency = Currency.getInstance(currencyIsoCode);
-        Map<String, Object> documentFields = document.getFields();
-        String documentId = (String) CoveoFieldValueResolverUtils.resolveFieldValue(COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE, documentFields, locale, currency);
-        // If the value is still blank at this point we are unable to build the document
-        if (StringUtils.isBlank(documentId)) {
-            LOG.warn("SnDocument with id " + document.getId() + " does not have a " + COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE + " field, will not push this document");
+        DocumentInfos documentInfos = getInfosAndValidateDocument(document, languageIsoCode, currencyIsoCode);
+        if (!documentInfos.isValid()) {
             return null;
         }
 
-        String documentName = (String) CoveoFieldValueResolverUtils.resolveFieldValue("name", documentFields, locale, currency);
-        // If the value is still blank at this point we are unable to build the document
-        if (StringUtils.isBlank(documentName)) {
-            LOG.warn("SnDocument with id " + document.getId() + " does not have a name field, will not push this document");
-            return null;
-        }
+        Map<String, Object> values = resolveFieldValue(document, documentInfos);
+        addEcProductIdToValues(document, document.getFields(), documentInfos.locale(), documentInfos.currency(), values);
+        DocumentBuilder documentBuilder = new DocumentBuilder(documentInfos.documentId(), documentInfos.documentName()).withMetadata(values);
 
-        Map<String, Object> fields = document.getFields();
-        Map<String, Object> values = new HashMap<>();
-        for (Map.Entry<String, Object> field : fields.entrySet()) {
-            Object fieldValue = CoveoFieldValueResolverUtils.resolveFieldValue(field.getValue(), locale, currency);
-            if (fieldValue != null && !Objects.equals(fieldValue, "")) {
-                values.put(field.getKey(), fieldValue);
-            } else if (LOG.isDebugEnabled()) {
-                LOG.debug("Field " + field.getKey() + " is empty or null, will not push this field for document " + documentId);
-            }
-        }
-        addEcProductIdToValues(document, documentFields, locale, currency, values);
-        DocumentBuilder documentBuilder = new DocumentBuilder(documentId, documentName).withMetadata(values);
-
-        String coveoClickableUri = (String) CoveoFieldValueResolverUtils.resolveFieldValue(COVEO_URI_TYPE_INDEX_ATTRIBUTE, documentFields, locale, currency);
+        String coveoClickableUri = (String) CoveoFieldValueResolverUtils.resolveFieldValue(COVEO_URI_TYPE_INDEX_ATTRIBUTE, document.getFields(), documentInfos.locale(), documentInfos.currency());
         if (!StringUtils.isBlank(coveoClickableUri)) {
             documentBuilder.withClickableUri(coveoClickableUri);
         }
         return documentBuilder;
+    }
+
+    private List<PartialUpdateDocument> createCoveoPartialDocument(SnDocument document, String languageIsoCode, String currencyIsoCode) {
+        DocumentInfos documentInfos = getInfosAndValidateDocument(document, languageIsoCode, currencyIsoCode);
+        if (!documentInfos.isValid()) {
+            return new ArrayList<>();
+        }
+
+        List<PartialUpdateDocument> partialUpdateDocuments = new ArrayList<>();
+        List<String> partialFieldsToUpdate = document.getFields().keySet().stream().filter(key -> !COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE.equals(key) && !COVEO_NAME_INDEX_ATTRIBUTE.equals(key)).toList();
+
+        for (String partialFieldToUpdate : partialFieldsToUpdate) {
+            Map<String, Object> values = resolveFieldValue(document, documentInfos);
+            // Only fieldValueReplace is supported for now
+            partialUpdateDocuments.add(new PartialUpdateDocument(documentInfos.documentId(), PartialUpdateOperator.FIELDVALUEREPLACE, partialFieldToUpdate, values.get(partialFieldToUpdate)));
+        }
+
+        return partialUpdateDocuments;
+    }
+
+    private Map<String, Object> resolveFieldValue(SnDocument document, DocumentInfos documentInfo) {
+        Map<String, Object> values = new HashMap<>();
+        for (Map.Entry<String, Object> field : document.getFields().entrySet()) {
+            Object fieldValue = CoveoFieldValueResolverUtils.resolveFieldValue(field.getValue(), documentInfo.locale(), documentInfo.currency());
+            if (fieldValue != null && !Objects.equals(fieldValue, "")) {
+                values.put(field.getKey(), fieldValue);
+            } else if (LOG.isDebugEnabled()) {
+                LOG.debug("Field " + field.getKey() + " is empty or null, will not push this field for document " + documentInfo.documentId());
+            }
+        }
+        return values;
+    }
+
+    private DocumentInfos getInfosAndValidateDocument(SnDocument document, String languageIsoCode, String currencyIsoCode) {
+        Locale locale = commonI18NService.getLocaleForIsoCode(languageIsoCode);
+        Currency currency = Currency.getInstance(currencyIsoCode);
+        Map<String, Object> documentFields = document.getFields();
+
+        String documentId = (String) CoveoFieldValueResolverUtils.resolveFieldValue(COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE, documentFields, locale, currency);
+        if (StringUtils.isBlank(documentId)) {
+            LOG.warn("SnDocument with id " + document.getId() + " does not have a " + COVEO_DOCUMENT_ID_INDEX_ATTRIBUTE + " field, will not push this document");
+            return new DocumentInfos(false, null, null, null, null);
+        }
+
+        String documentName = (String) CoveoFieldValueResolverUtils.resolveFieldValue("name", documentFields, locale, currency);
+        if (StringUtils.isBlank(documentName)) {
+            LOG.warn("SnDocument with id " + document.getId() + " does not have a name field, will not push this document");
+            return new DocumentInfos(false, null, null, null, null);
+        }
+
+        return new DocumentInfos(true, documentId, documentName, locale, currency);
+    }
+
+    private record DocumentInfos(boolean isValid, String documentId, String documentName, Locale locale, Currency currency) {
     }
 
     protected static void addEcProductIdToValues(SnDocument document, Map<String, Object> documentFields, Locale locale, Currency currency, Map<String, Object> values) {
